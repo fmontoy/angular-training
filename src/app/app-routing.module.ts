b7192210191e6f60209923a2a9d862eb094660b1
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-08-28 16:42:46 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { HomeComponent } from './screens/dashboard/screens/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent, children: [
+    { path: 'home', component: HomeComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { NavbarComponent } from './screens/dashboard/components/navbar/navbar.component';
+import { FooterComponent } from './screens/dashboard/components/footer/footer.component';
+import { HomeComponent } from './screens/dashboard/screens/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
